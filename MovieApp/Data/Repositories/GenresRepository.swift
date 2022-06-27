17672f0f9153f80f055ecb1b45b7fda52b69fc55
Paul Willis <paul.willis@mkodo.com>
2022-06-27 17:00:01 +0100

    Creates basic and unimplemented GenresRepository and associated empty test file

---
+++ MovieApp/Data/Repositories/GenresRepository.swift
@@ -0,0 +1,14 @@
+//
+//  GenresRepository.swift
+//  MovieApp
+//
+//  Created by Paul on 27/06/2022.
+//
+
+import Foundation
+
+private class GenresRepository: GenresRepositoryProtocol {
+    func getMovieGenres() -> [Genre] {
+        return []
+    }
+}
---
+++ MovieAppTests/Data/Repositories/GenresRepositoryTests.swift
@@ -0,0 +1,15 @@
+//
+//  GenresRepository.swift
+//  MovieAppTests
+//
+//  Created by Paul on 27/06/2022.
+//
+
+import Foundation
+import XCTest
+@testable import MovieApp
+
+class GenresRepositoryTests: XCTestCase {
+    
+    
+}
