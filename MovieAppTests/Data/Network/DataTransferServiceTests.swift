7e1b3e2fcd6e52923a9e22c85205714705d465b5
Paul Willis <paul.willis@mkodo.com>
2022-06-30 23:40:28 +0100

    Refactored DataTransferServiceTests to be more readable and flexible

--- MovieAppTests/Data/Network/DataTransferServiceTests.swift
+++ MovieAppTests/Data/Network/DataTransferServiceTests.swift
@@ -11,65 +11,132 @@ import XCTest
 
 class DataTransferServiceTests: XCTestCase {
     
+    private var networkService: NetworkServiceMock?
+    private var sut: DataTransferService?
+    
+    private var expectedReturnedURLSessionTask: URLSessionTask?
+    private var returnedURLSessionTask: URLSessionTask?
+    
+    private var sentURLRequest: URLRequest?
+    private var urlRequestReceivedByNetworkService: URLRequest?
     
     // MARK: - Setup
     
+    override func setUp() {
+        super.setUp()
+        
+        
+    }
     
+    override func tearDown() {
+        self.networkService = nil
+        self.sut = nil
+        
+        self.expectedReturnedURLSessionTask = nil
+        self.returnedURLSessionTask = nil
+        
+        self.sentURLRequest = nil
+        self.urlRequestReceivedByNetworkService = nil
+        
+        super.tearDown()
+    }
     
     // MARK: - Tests
     
-    func test_DataTransferService_whenPerformRequest_shouldReturnOptionalURLSessionTask() {
+    func test_DataTransferService_whenPerformRequest_shouldReturnURLSessionTask() {
         //given
-        let networkService = NetworkServiceMock()
-        let sut = DataTransferService(networkService: networkService)
+        givenDataTransferServiceInitialised()
         
         // when
-        let response = sut.request(request: URLRequest(url: URL(string: "www.expectedReturnValue.com")!))
+        whenNetworkRequestIsPerformed()
         
         // then
-        XCTAssertNotNil(response)
+        thenEnsureReturnsURLSessionTask()
     }
     
     func test_DataTransferService_whenPerformRequest_shouldCallNetworkServiceExactlyOnce() {
         // given
-        let networkService = NetworkServiceMock()
-        let sut = DataTransferService(networkService: networkService)
+        givenDataTransferServiceInitialised()
         
         // when
-        let _ = sut.request(request: URLRequest(url: URL(string: "www.expectedReturnValue.com")!))
+        whenNetworkRequestIsPerformed()
         
         // then
-        XCTAssertEqual(networkService.requestCallsCount, 1)
+        thenEnsureNetworkServiceCalledExactlyOnce()
     }
     
     func test_DataTransferService_whenPerformRequest_shouldReturnURLSessionTaskFromNetworkService() {
         // given
-        let networkService = NetworkServiceMock()
-        let expectedReturnValue = URLSessionTask()
-        networkService.requestReturnValue = expectedReturnValue
-        let sut = DataTransferService(networkService: networkService)
-        
+        givenDataTransferServiceInitialised()
+        givenExpectedNetworkRequestResponse()
+                
         // when
-        let actualReturnValue = sut.request(request: URLRequest(url: URL(string: "www.expectedReturnValue.com")!))
+        whenNetworkRequestIsPerformed()
         
         // then
-        XCTAssertEqual(expectedReturnValue, actualReturnValue)
+        thenEnsureReturnsURLSessionTaskFromNetworkService()
     }
     
     func test_DataTransferService_whenPerformRequest_shouldPassRequestToNetworkService() {
         // given
-        let networkService = NetworkServiceMock()
-        let expectedReturnValue = URLRequest(url: URL(string: "www.expectedReturnValue.com")!)
-        
-        let sut = DataTransferService(networkService: networkService)
-        
+        givenDataTransferServiceInitialised()
+                
         // when
-        let _ = sut.request(request: expectedReturnValue)
-        let actualReturnValue = networkService.requestReceivedRequest
+        whenNetworkRequestIsPerformed()
         
         // then
-        XCTAssertEqual(expectedReturnValue, actualReturnValue)
+        thenEnsureRequestIsPassedToNetworkService()
     }
     
     // MARK: - Given
+    
+    private func givenDataTransferServiceInitialised() {
+        self.networkService = NetworkServiceMock()
+        guard let networkService = networkService else { return }
+        self.sut = DataTransferService(networkService: networkService)
+    }
+    
+    private func givenExpectedNetworkRequestResponse(of urlSessionTask: URLSessionTask? = nil) {
+        self.expectedReturnedURLSessionTask = urlSessionTask ?? URLSessionTask()
+        self.networkService?.requestReturnValue = expectedReturnedURLSessionTask
+    }
+    
+    // MARK: - When
+    
+    private func whenNetworkRequestIsPerformed() {
+        self.returnedURLSessionTask = sut?.request(request: self.urlRequest()!)
+    }
+    
+    // MARK: - Then
+    
+    private func thenEnsureReturnsURLSessionTask() {
+        XCTAssertNotNil(self.returnedURLSessionTask)
+    }
+    
+    private func thenEnsureNetworkServiceCalledExactlyOnce() {
+        XCTAssertEqual(self.networkService?.requestCallsCount, 1)
+    }
+    
+    private func thenEnsureReturnsURLSessionTaskFromNetworkService() {
+        guard
+            let expectedReturnedURLSessionTask = self.expectedReturnedURLSessionTask,
+            let returnedURLSessionTask = self.returnedURLSessionTask
+        else {
+            XCTFail("should not be nil")
+            return
+        }
+        
+        XCTAssertEqual(expectedReturnedURLSessionTask, returnedURLSessionTask)
+    }
+    
+    private func thenEnsureRequestIsPassedToNetworkService() {
+        XCTAssertEqual(urlRequest(), networkService?.requestReceivedRequest)
+    }
+    
+    // MARK: - Helpers
+    
+    private func urlRequest() -> URLRequest? {
+        URLRequest(url: URL(string: "www.expectedReturnValue.com")!)
+    }
+    
 }
