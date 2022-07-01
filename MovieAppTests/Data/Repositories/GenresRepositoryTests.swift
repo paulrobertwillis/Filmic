ea59c890bdada2a2e88f4b2aaa62dbfba1645c9f
Paul Willis <paul.willis@mkodo.com>
2022-07-01 21:57:50 +0100

    Removed commented-out code from GenresRepositoryTests

--- MovieAppTests/Data/Repositories/GenresRepositoryTests.swift
+++ MovieAppTests/Data/Repositories/GenresRepositoryTests.swift
@@ -161,23 +161,3 @@ class GenresRepositoryTests: XCTestCase {
         return try self.resultValue?.get()
     }
 }
-
-
-//var requestCallsCount = 0
-//var requestReturnValue: URLSessionTask? = URLSessionTask()
-//
-//// request parameter
-//var requestReceivedRequest: URLRequest?
-//
-//// completion parameter
-//var requestCompletionReturnValue: ResultValue = .success(nil)
-//
-//func request(request: URLRequest, completion: CompletionHandler) -> URLSessionTask? {
-//    self.requestCallsCount += 1
-//
-//    self.requestReceivedRequest = request
-//
-//    completion(requestCompletionReturnValue)
-//
-//    return requestReturnValue
-//}
