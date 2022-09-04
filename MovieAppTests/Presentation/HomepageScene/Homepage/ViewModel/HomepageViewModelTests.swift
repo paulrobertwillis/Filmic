cd95b2e70721d42116c69d6dadc39c1f1cf3e749
Paul Willis <paul.willis@mkodo.com>
2022-09-04 09:46:37 +0100

    Moves ImagesDataTransferServiceMock to its own file; begins creating tests for HomepageViewModel

--- MovieAppTests/Presentation/HomepageScene/Homepage/ViewModel/HomepageViewModelTests.swift
+++ MovieAppTests/Presentation/HomepageScene/Homepage/ViewModel/HomepageViewModelTests.swift
@@ -5,4 +5,25 @@
 //  Created by Paul on 02/09/2022.
 //
 
-import Foundation
+import XCTest
+@testable import MovieApp
+
+class HomepageViewModelTests: XCTestCase {
+    
+    // MARK: - Private Properties
+    
+    private var sut: HomepageViewModel!
+    
+    
+    // MARK: - Lifecycle
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    
+}
